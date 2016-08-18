8f235bd4152acf86a0a8c55f0e0090382ef20979
os <osirisbest@gmail.com>
2016-08-18 20:48:57 +0300

    Deletion work

--- src/main/java/HttpSQL/HttpSQL/HttpSQL/SQLDAO.java
+++ src/main/java/HttpSQL/HttpSQL/HttpSQL/SQLDAO.java
@@ -5,14 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class SQLDAO {
 	private static SQLDAO mapper = new SQLDAO();
 
 	private static JdbcTemplate jdbcTemplate;
-	private MapSqlParameterSource parametr = new MapSqlParameterSource();
+	// private MapSqlParameterSource parametr = new MapSqlParameterSource();
 
 	SQLDAO() {
 
@@ -34,6 +33,11 @@ public class SQLDAO {
 		jdbcTemplate.execute(sql);
 	}
 
+	public void delete(int ID) {
+		String sql = "delete from `bookmanager`.`books` where ID=?";
+		jdbcTemplate.update(sql, new Object[] { ID });
+	}
+
 	public void insert(String title, String author, String price) {
 		String sql = "INSERT INTO `bookmanager`.`books` (`BOOK_TITLE`, `BOOK_AUTHOR`, `BOOK_PRICE`) VALUES (?, ?, ?);";
 		jdbcTemplate.update(sql, new Object[] { title, author, price });
@@ -42,12 +46,12 @@ public class SQLDAO {
 	public List<book> getAll() {
 
 		String sql = "select * from `bookmanager`.`books` order by ID DESC";
-		List list = jdbcTemplate.queryForList(sql);
+		List<?> list = jdbcTemplate.queryForList(sql);
 
 		List<book> booklist = new ArrayList<book>();
 
 		for (Object el : list) {
-			HashMap map = (HashMap) el;
+			HashMap<?, ?> map = (HashMap<?, ?>) el;
 			//
 			// el.toString()
 			booklist.add(new book((Integer) map.get("ID"), (String) map.get("BOOK_TITLE"),
